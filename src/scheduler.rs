9680b7bd5a29306d8aa133904968e016d4289dbf
Antoine Lavallée <antoine.lavallee@flyable.dev>
2024-04-14 02:06:55 -0400

    Block system to control flow

--- src/scheduler.rs
+++ src/scheduler.rs
@@ -1,21 +1,43 @@
+use std::collections::HashMap;
 use crate::{Task};
 use crate::commands::{EntityCommand, EntityCommands};
 use crate::schedule::sorted_task::SortedTask;
 use crate::world::World;
 
+struct SchedulerBlock {
+    tasks: Vec<SortedTask>,
+}
+
 pub struct Scheduler {
-    schedule: Vec<SortedTask>,
+    blocks: HashMap<u32, SchedulerBlock>,
     is_sorted: bool,
 }
 
 impl Scheduler {
     pub fn new() -> Self {
-        Self { schedule: vec![], is_sorted: false }
+        Self { blocks: HashMap::new(), is_sorted: false }
     }
 
     pub fn add_task<T: Task + 'static>(&mut self, new_task: T) {
+        let new_task = SortedTask::new(Box::new(new_task));
         self.is_sorted = false;
-        self.schedule.push(SortedTask::new(Box::new(new_task)))
+        if let Some(block) = self.blocks.get_mut(&0) {
+            block.tasks.push(new_task);
+        } else {
+            let new_block = SchedulerBlock { tasks: vec![new_task] };
+            self.blocks.insert(0, new_block);
+        }
+    }
+
+    pub fn add_task_in_block<T: Task + 'static>(&mut self, block_id: u32, new_task: T) {
+        let new_task = SortedTask::new(Box::new(new_task));
+        self.is_sorted = false;
+        if let Some(block) = self.blocks.get_mut(&block_id) {
+            block.tasks.push(new_task);
+        } else {
+            let new_block = SchedulerBlock { tasks: vec![new_task] };
+            self.blocks.insert(block_id, new_block);
+        }
     }
 
     pub fn run(&mut self, world: &mut World) {
@@ -23,25 +45,26 @@ impl Scheduler {
             self.sort_tasks();
         }
 
+        for (_, block) in self.blocks.iter() {
+            for task in &block.tasks {
+                let mut commands = EntityCommands::new();
+                {
+                    let (comps_manager, entities, res) = world.managers();
+                    task.task().run(comps_manager, &mut commands, res);
+                }
 
-        for task in &self.schedule {
-            let mut commands = EntityCommands::new();
-            {
-                let (comps_manager, entities, res) = world.managers();
-                task.task().run(comps_manager, &mut commands, res);
-            }
-            
-            for cmd in commands.take_commands().into_iter() {
-                match cmd {
-                    EntityCommand::NewEntity(components) => {
-                        for (comp_type_id, comp) in components {
-                            let new_entity = world.create();
-                            let (comps_manager, _, _) = world.managers();
-                            comps_manager.insert_by_comp_id(comp_type_id, new_entity, comp);
+                for cmd in commands.take_commands().into_iter() {
+                    match cmd {
+                        EntityCommand::NewEntity(components) => {
+                            for (comp_type_id, comp) in components {
+                                let new_entity = world.create();
+                                let (comps_manager, _, _) = world.managers();
+                                comps_manager.insert_by_comp_id(comp_type_id, new_entity, comp);
+                            }
                         }
-                    }
-                    EntityCommand::RemoveEntity(entity_id) => {
-                        world.release(entity_id);
+                        EntityCommand::RemoveEntity(entity_id) => {
+                            world.release(entity_id);
+                        }
                     }
                 }
             }
@@ -50,9 +73,11 @@ impl Scheduler {
 
     fn sort_tasks(&mut self) {
         self.is_sorted = true;
-        for i in 0..self.schedule.len() {
-            let depend_index = self.schedule[i].find_dependencies(&self.schedule[..i]);
-            self.schedule[i].set_depends(depend_index);
+        for (_, block) in self.blocks.iter_mut() {
+            for i in 0..block.tasks.len() {
+                let depend_index = block.tasks[i].find_dependencies(&block.tasks[..i]);
+                block.tasks[i].set_depends(depend_index);
+            }
         }
     }
 }
